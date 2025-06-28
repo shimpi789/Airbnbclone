9985351b51db73bfe45b72f97298c568d13418a9
Shimpi Rajawat <shimpirajawat360@gmail.com>
2025-06-28 11:29:30 +0530

    Update auth.controller.js

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -13,9 +13,9 @@ export const signUp = async (req,res) => {
         let user  = await User.create({name,email,password:hashPassword})
         let token = await genToken(user._id)
         res.cookie("token",token,{
-            httpOnly : "true",
-            secure : process.env.NODE_ENVIRONMENT = "production",
-            sameSite : "strict",
+            httpOnly : true,
+            secure :true,
+            sameSite : "none",
             maxAge : 7*24*60*60*1000
         })
 
@@ -44,9 +44,9 @@ export const login = async (req,res) => {
         }
         let token = await genToken(user._id)
         res.cookie("token",token,{
-            httpOnly : "true",
-            secure : process.env.NODE_ENVIRONMENT = "production",
-            sameSite : "strict",
+            httpOnly : true,
+            secure : true,
+            sameSite : "none",
             maxAge : 7*24*60*60*1000
         })
 
@@ -66,8 +66,8 @@ export const logOut = async (req, res) => {
     try {
       res.clearCookie("token", {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
+        secure: true,
+        sameSite: "none",
       });
   
       return res.status(200).json({ message: "Logout successfully" });
